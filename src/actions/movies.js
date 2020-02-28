8e9fefb928a15f577c3bff2100942e0e9e773322
Kiryl Chetyrbak <kirylchetyrbak@Kiryls-MacBook-Pro.local>
2020-02-27 19:56:01 -0800

    added reducer to details screen

--- src/actions/movies.js
+++ src/actions/movies.js
@@ -1,9 +1,13 @@
 import {
   LOAD_FRONT_RESULT,
   LOAD_FRONT_RESULT_SUCCESS,
-  LOAD_FRONT_RESULT_FAILURE
+  LOAD_FRONT_RESULT_FAILURE,
+  LOAD_DETAILS,
+  LOAD_DETAILS_SUCCESS,
+  RATE_MOVIE
 } from './types/movies';
-import { getFrontpage } from '../api/movielens';
+
+import { getFrontpage, getMovie, rate, unhide } from '../api/movielens';
 
 export const loadFront = () => {
   return async dispatch => {
@@ -16,6 +20,36 @@ export const loadFront = () => {
       dispatch(loadFrontFailure(error.message));
     }
   };
+};
+
+export const loadDetails = movieId => async dispatch => {
+  dispatch({ type: LOAD_DETAILS });
+  const {
+    data: {
+      movieDetails: { movie, movieUserData }
+    }
+  } = await getMovie(null, movieId);
+  dispatch({ type: LOAD_DETAILS_SUCCESS, payload: { movie, movieUserData } });
+};
+
+export const rateMovie = (movieId, rating) => async dispatch => {
+  dispatch({ type: RATE_MOVIE, payload: { movieId, rating } });
+  const {
+    data: {
+      movieDetails: { movieUserData }
+    }
+  } = await getMovie(null, movieId);
+  rate(null, {
+    movieId,
+    rating,
+    prediction: movieUserData.prediction,
+    previousRating: movieUserData.rating
+  });
+};
+
+export const unrateMovie = movieId => dispatch => {
+  unhide(null, movieId);
+  dispatch({ type: RATE_MOVIE, payload: { movieId, rating: null } });
 };
 
 const loadFrontStart = () => ({ type: LOAD_FRONT_RESULT });
