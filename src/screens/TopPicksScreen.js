5f04e48ac62dfd85b6140e086f7fec36c4cdf6df
Kiryl Chetyrbak <kirylchetyrbak@Kiryls-MBP.attlocal.net>
2020-02-28 13:26:27 -0800

    Cleaned up top picks screen

--- src/screens/TopPicksScreen.js
+++ src/screens/TopPicksScreen.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { View, FlatList, SafeAreaView } from 'react-native';
-import { Text } from 'react-native-elements';
 import MoviesGrid from '../components/MoviesGrid';
 import { topPicks } from '../api/movielens';
 const TopPicksScreen = ({ params }) => {
   return (
-    // <SafeAreaView>
     <MoviesGrid
       title="Top Picks"
       dataLoader={page => topPicks(null, { page })}
     ></MoviesGrid>
-    // </SafeAreaView>
   );
 };
 export default TopPicksScreen;
